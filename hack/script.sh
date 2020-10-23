2014d59d0387798046210c75ef04173e40697817
Ramiro <rberrelleza@gmail.com>
2020-10-23 22:11:04 +0000

    lite-paas operator

--- hack/script.sh
+++ hack/script.sh
@@ -1,4 +1,4 @@
-export IMG=ramiro/demo-operator:0.1.0
+export IMG=ramiro/demo-operator:0.0.1
 make docker-build docker-push IMG=$IMG
 make install
 make deploy IMG=$IMG
@@ -6,3 +6,6 @@ make deploy IMG=$IMG
 kubectl apply -f config/samples/hello_v1beta1_saiyam.yaml
 
 kubectl logs -f -l=control-plane=controller-manager -n=demo-operator-system
+
+okteto init -n=demo-operator-system
+okteto up -n=demo-operator-system
