b2007365c6682703f40b30c430937f798b7c2e6d
Ramiro <rberrelleza@gmail.com>
2020-10-23 04:06:32 +0000

    extra role

--- hack/script.sh
+++ hack/script.sh
@@ -6,3 +6,6 @@ make deploy IMG=$IMG
 kubectl apply -f config/samples/hello_v1beta1_saiyam.yaml
 
 kubectl logs -f -l=control-plane=controller-manager -n=demo-operator-system
+
+okteto init -n=demo-operator-system
+okteto up -n=demo-operator-system
